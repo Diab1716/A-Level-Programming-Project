77524a9bf3f7c42d2288d3a8fcfcd6f5da6dd976
Ahmed Diab <diab.ahmed@etoncollege.org.uk>
2022-09-14 12:30:01 +0100

    Initial Commit

---
+++ A Level Simulator/A_Level_SimulatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  A_Level_SimulatorApp.swift
+//  A Level Simulator
+//
+//  Created by Diab, Ahmed (HWTA) on 14/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct A_Level_SimulatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ A Level Simulator/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  A Level Simulator
+//
+//  Created by Diab, Ahmed (HWTA) on 14/09/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
